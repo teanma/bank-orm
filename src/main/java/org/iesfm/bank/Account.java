3963bfc184dbf8289661dcbdd6ec5f0e08595951
teanma <mardaleteodorandrei@gmail.com>
2021-12-15 14:15:08 +0100

    Update

--- src/main/java/org/iesfm/bank/Account.java
+++ src/main/java/org/iesfm/bank/Account.java
@@ -3,6 +3,7 @@ package org.iesfm.bank;
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Account {
@@ -10,12 +11,14 @@ public class Account {
     @Id
     @Column(name = "iban")
     private String accountIban;
-    @OneToMany
-    @JoinColumn(name = "owner_id", referencedColumnName = "id")
+    @Column(name = "owner_id")
     private int ownerId;
     private double balance;
     @Column(name = "open_date")
     private Date openDate;
+    @OneToMany
+    @JoinColumn(name = "iban", referencedColumnName = "iban")
+    private Set<Movement> movements;
 
     public String getAccountIban() {
         return accountIban;
@@ -49,16 +52,24 @@ public class Account {
         this.openDate = openDate;
     }
 
+    public Set<Movement> getMovements() {
+        return movements;
+    }
+
+    public void setMovements(Set<Movement> movements) {
+        this.movements = movements;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Account account = (Account) o;
-        return ownerId == account.ownerId && Double.compare(account.balance, balance) == 0 && Objects.equals(accountIban, account.accountIban) && Objects.equals(openDate, account.openDate);
+        return ownerId == account.ownerId && Double.compare(account.balance, balance) == 0 && Objects.equals(accountIban, account.accountIban) && Objects.equals(openDate, account.openDate) && Objects.equals(movements, account.movements);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(accountIban, ownerId, balance, openDate);
+        return Objects.hash(accountIban, ownerId, balance, openDate, movements);
     }
 }
